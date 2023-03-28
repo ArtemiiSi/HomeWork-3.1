0edc74351f8d2c2b73aa601f91c032452815b72b
Artemii Sinitsybgit config --global user.name Artemii <fossil545@gmail.com>
2023-03-29 00:52:28 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8_000;
+        int bonusMiles = ticketPrice / 20;
+        System.out.println(bonusMiles);
+
+    }
+}
